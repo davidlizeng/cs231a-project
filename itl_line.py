56c34a0db2f6ac2f6f7e45c420a83cfa3c7e8ed4
Christopher Wong <chriswong205@gmail.com>
2015-03-07 02:27:56 -0800

    Started working on parsing line into words. TODO: Standardize line height and get better bounding boxes for words.

--- itl_line.py
+++ itl_line.py
@@ -1,15 +1,15 @@
-# Parse one paragraph of text
+# Parse one line of text
 import cv
 import cv2
 import numpy as np
 import sys
 
 DEBUG = False
-IMAGE_FILE = 'images/paragraph1.png'
+IMAGE_FILE = 'images/line1.png'
 [IMAGE_NAME, EXTENSION] = IMAGE_FILE.split('.')
 
-# Paragraph image img
-def parseParagraph(img):
+# Line image img
+def parseLine(img):
     img_gray = cv2.cvtColor(img, cv.CV_BGR2GRAY)
     blur = cv2.GaussianBlur(img_gray, (5, 5), 0)
     img_lap = cv2.Laplacian(img_gray, cv2.CV_8U)
@@ -18,12 +18,12 @@ def parseParagraph(img):
         cv2.imwrite(IMAGE_NAME + '-thresh.' + EXTENSION, img_threshold)
 
     # Blur in the horizontal direction to get lines
-    element = cv2.getStructuringElement(cv2.MORPH_RECT, (25, 1))
+    element = cv2.getStructuringElement(cv2.MORPH_RECT, (5, 10))
     morphed = cv2.morphologyEx(img_threshold, cv.CV_MOP_CLOSE, element)
     if DEBUG:
         cv2.imwrite(IMAGE_NAME + '-morph.' + EXTENSION, morphed)
 
-    # Use RETR_EXTERNAL to remove boxes that are completely contained by the line
+    # Use RETR_EXTERNAL to remove boxes that are completely contained by the word
     contours, hierarchy = cv2.findContours(morphed, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     boundRects = []
     for i in xrange(len(contours)):
@@ -31,33 +31,33 @@ def parseParagraph(img):
         boundRect = cv2.boundingRect(contourPoly)
         boundRects.append(boundRect)
 
-    # Filter bounding rectangles that are not an entire line
-    # Take the maximum height among all bounding boxes
-    # Remove those boxes that have height less than 25% of the maximum
-    maxHeight = -1
-    for rect in boundRects:
-        maxHeight = max(rect[3], maxHeight)
-    heightThresh = .25 * maxHeight
-    boundRects = [rect for rect in boundRects if rect[3] > heightThresh]
+    # # Filter bounding rectangles that are not an entire line
+    # # Take the maximum height among all bounding boxes
+    # # Remove those boxes that have height less than 25% of the maximum
+    # maxHeight = -1
+    # for rect in boundRects:
+    #     maxHeight = max(rect[3], maxHeight)
+    # heightThresh = .25 * maxHeight
+    # boundRects = [rect for rect in boundRects if rect[3] > heightThresh]
 
     if DEBUG:
         for rect in boundRects:
             cv2.rectangle(img, (rect[0], rect[1]), (rect[0] + rect[2], rect[1] + rect[3]), (0, 255, 0))
         cv2.imwrite(IMAGE_NAME + '-bounds.' + EXTENSION, img)
-        print '%d lines found in %s' % (len(boundRects), IMAGE_FILE)
+        print '%d words found in %s' % (len(boundRects), IMAGE_FILE)
 
-    # Extract lines from paragraph
-    lines = []
-    for rect in boundRects:
+    # Extract words from paragraph
+    words = []
+    for word in boundRects:
         [x, y, w, h] = rect
-        line = img[y:(y+h), x:(x+w)]
-        lines.append(line)
+        word = img[y:(y+h), x:(x+w)]
+        words.append(word)
 
-    for line in lines:
-        # TODO do something here
-        if DEBUG:
-            cv2.imshow('Line', line)
-            cv2.waitKey(0)
+    # for word in words:
+    #     # TODO do something here
+    #     if DEBUG:
+    #         cv2.imshow('Word', word)
+    #         cv2.waitKey(0)
 
     return boundRects
 
@@ -66,7 +66,7 @@ def test():
     global DEBUG
     DEBUG = True
     img = cv2.imread(IMAGE_FILE)
-    parseParagraph(img)
+    parseLine(img)
 
 if __name__ == "__main__":
     test()
