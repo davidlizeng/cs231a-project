a92c34ff8d39dd9a39e585ef1c81c74911a91995
Christopher Wong <chriswong205@gmail.com>
2015-03-08 20:45:02 -0700

    Implemented line parsing into words.

--- itl_line.py
+++ itl_line.py
@@ -11,14 +11,14 @@ IMAGE_FILE = 'images/line1.png'
 # Line image img
 def parseLine(img):
     img_gray = cv2.cvtColor(img, cv.CV_BGR2GRAY)
-    blur = cv2.GaussianBlur(img_gray, (5, 5), 0)
+    # blur = cv2.GaussianBlur(img_gray, (5, 5), 0)
     img_lap = cv2.Laplacian(img_gray, cv2.CV_8U)
-    unused, img_threshold = cv2.threshold(img_lap, 0, 255, cv.CV_THRESH_OTSU + cv.CV_THRESH_BINARY)
+    unused, img_threshold = cv2.threshold(img_lap, 0, 255, cv.CV_THRESH_BINARY)
     if DEBUG:
         cv2.imwrite(IMAGE_NAME + '-thresh.' + EXTENSION, img_threshold)
 
     # Blur in the horizontal direction to get lines
-    element = cv2.getStructuringElement(cv2.MORPH_RECT, (5, 10))
+    element = cv2.getStructuringElement(cv2.MORPH_RECT, (5, 20))
     morphed = cv2.morphologyEx(img_threshold, cv.CV_MOP_CLOSE, element)
     if DEBUG:
         cv2.imwrite(IMAGE_NAME + '-morph.' + EXTENSION, morphed)
@@ -31,24 +31,29 @@ def parseLine(img):
         boundRect = cv2.boundingRect(contourPoly)
         boundRects.append(boundRect)
 
-    # # Filter bounding rectangles that are not an entire line
-    # # Take the maximum height among all bounding boxes
-    # # Remove those boxes that have height less than 25% of the maximum
-    # maxHeight = -1
-    # for rect in boundRects:
-    #     maxHeight = max(rect[3], maxHeight)
-    # heightThresh = .25 * maxHeight
-    # boundRects = [rect for rect in boundRects if rect[3] > heightThresh]
+    # We assume that the image is wrapped fairly tightly around the line
+    # We are only concerned with finding word boundaries, so extend the x-coordinate as
+    # far left as possible, and then extend the height to maximum
+    h = img.shape[0]
+    adjustedRects = []
+    currentLeftX = 0
+    rightPad = 2
+    for rect in boundRects:
+        x = currentLeftX
+        w = rect[2] + (rect[0] - currentLeftX) + rightPad
+        adjustedRect = (x, 0, w, h)
+        adjustedRects.append(adjustedRect)
+        currentLeftX = rect[0] + rect[2] + rightPad
 
     if DEBUG:
-        for rect in boundRects:
+        for rect in adjustedRects:
             cv2.rectangle(img, (rect[0], rect[1]), (rect[0] + rect[2], rect[1] + rect[3]), (0, 255, 0))
         cv2.imwrite(IMAGE_NAME + '-bounds.' + EXTENSION, img)
-        print '%d words found in %s' % (len(boundRects), IMAGE_FILE)
+        print '%d words found in %s' % (len(adjustedRects), IMAGE_FILE)
 
     # Extract words from paragraph
     words = []
-    for word in boundRects:
+    for word in adjustedRects:
         [x, y, w, h] = rect
         word = img[y:(y+h), x:(x+w)]
         words.append(word)
